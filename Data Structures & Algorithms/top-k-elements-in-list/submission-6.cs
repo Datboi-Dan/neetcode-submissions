166e7ec94ee7a31ca0b501be62a91d7d941df8e1
Datboi-Dan <datboidan961@gmail.com>
2026-06-25 14:33:34 -1000

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) 
+    {
+       
+        var freqs = new Dictionary<int, int>();
+
+        foreach (int num in nums)
+        {
+            
+            if (!freqs.ContainsKey(num))
+                freqs.Add(num, 0);
+
+            freqs[num]++;
+
+        }
+
+        var buckets = new Stack<int>[nums.Length];
+        foreach (KeyValuePair<int, int> kvp in freqs)
+        {
+            if (buckets[kvp.Value - 1] == null)
+                buckets[kvp.Value - 1] = new Stack<int>();
+            buckets[kvp.Value - 1].Push(kvp.Key); 
+        }
+
+        var topK = new int[k];
+        int posFromTop = 0;
+        
+        for (int i = 0; i < k; i++)
+        {
+            
+            while (buckets[buckets.Length - 1 - posFromTop] == null || buckets[buckets.Length - 1 - posFromTop].Count == 0)
+                posFromTop++;
+
+            topK[i] = buckets[buckets.Length - 1 - posFromTop].Pop();
+            
+        }
+
+        return topK;
+
+    }
+}
